90909d9f09bed6925dcd430c7ff8f19526ea44d9
Carlos Filoteo <c.filoteo12@gmail.com>
2020-12-18 08:41:21 -0700

    Add inquirer-multi-choice

--- src/prompt.ts
+++ src/prompt.ts
@@ -1,6 +1,6 @@
 import * as inquirer from "inquirer";
-import * as SearchBox from "inquirer-search-list";
+import * as MultiChoice from "inquirer-multi-choice";
 
-inquirer.registerPrompt("search-list", SearchBox);
+inquirer.registerPrompt("multi-choice", MultiChoice);
 
 export default inquirer.prompt;
